06fdee517bbcc6ee50c42ad91d28caf3e5a74d24
20revsined <44682153+20revsined@users.noreply.github.com>
2019-04-06 18:14:46 -0400

    Update dow_example.py

--- src/dow_example.py
+++ src/dow_example.py
@@ -12,9 +12,15 @@ end = '2016'
 
 pltdf = df[start:end]
 
+stkind = 0
+tmpdf = pltdf[stks[stkind]]
 plt.figure()
 plt.plot()
 
-tmpdf = pltdf[stks[0]]
-plt.plot(tmpdf.index,tmpdf.values)
+for stkind in range(len(stks)):
+  plt.plot(tmpdf.index,tmpdf.values)
+
+plt.title('Prices of ' + stks[stkind] + ' from ' + start + ' to ' + end)
+plt.xlabel('Dates')
+plt.ybael('Price')
 plt.show()
